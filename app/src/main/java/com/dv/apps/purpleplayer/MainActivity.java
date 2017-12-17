b5a96f2fb7798a862cb66e6e835a0fbfbfc9384e
Dhaval <dkillworld@gmail.com>
2017-12-17 14:58:38 +0530

    v2.3.4 TEST
    # Added swipe to change functionality
    # Returning start not sticky
    # Fixed connection timeour for fetching lyrics
    # Added a security patch
    
    v2.3.3
    
    # Fixed Themes
    # Major Update
    # Updated resources for 4.0 to 5.0 devices
    # Added back background_list
    # Using Picasso to load images
    
    v2.3.0
    
    # Full custom theme support
    # Fixed Crash

--- app/src/main/java/com/dv/apps/purpleplayer/MainActivity.java
+++ app/src/main/java/com/dv/apps/purpleplayer/MainActivity.java
@@ -195,7 +195,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playPauseMain = (ImageButton) findViewById(R.id.playPauseMain);
         tvMain = (TextView) findViewById(R.id.tvMain);
 
-        authenticate(); //Checking for license and Piracy
+        purple();
     }
 
     public void buildTransportControls(){
@@ -485,12 +485,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
-    private void authenticate(){
+    private void purple(){
         if (BuildConfig.APPLICATION_ID.equals("com.dv.apps.purpleplayerpro")) {
             checker = new PiracyChecker(this)
                     .enableGooglePlayLicensing("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgBT+tKXqMH4FEejIu9Zhbs6+1N/UXFPN7TK11PYzkYe5qSvQnfENkdjXfJQ55h2aAbMn1jOXXB5xQwDHyRE2VNlrGBIplIRPFfDpZ4Vl/2niCwseLbke9VetHGIgx9vROBsJs9QMWJC0/yphxPqARXNJ+uYkQg164ZXaLcAl7/7pOxucZ9DKN0lbIqwE8eysFr6gcCeVutGfn5tDya5+cFj9zMGq6ImQSaCPTcWXm4/up2HyASKVw9TYuCgvGRvVF1BrP6ifs6uXFxZvK1mYCnVHGXPhAlQjlnTMp2k8Wy/KJdgCYRYjeMfvm+Z/KOp2mLZBW5QAc6Aro4jG9Pxr+wIDAQAB")
                     .saveResultToSharedPreferences(preferences, "valid_license")
-                    .enableSigningCertificate("uxh/RlppBQNr/6nlf3bO4UmKmNg=")
+//                    .enableSigningCertificate("uxh/RlppBQNr/6nlf3bO4UmKmNg=")
 //                    .enableUnauthorizedAppsCheck()
 //                    .blockIfUnauthorizedAppUninstalled(preferences, "app_unauth")
                     .display(Display.ACTIVITY);
